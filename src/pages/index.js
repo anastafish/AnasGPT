1a1d23c522eb4a9f76951d061e387abf6bcd2b41
anastafish <anastafish24@gmail.com>
2023-06-17 19:23:42 +0300

    improve mobile experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,14 @@ export default function Chat() {
   }, [messages]);
 
   return (
-    <div className="flex flex-row items-start justify-center w-[100vw]">
+    <div className="flex flex-col items-center justify-center w-[100vw]">
       <Head>
         <title>AnasGPT</title>
       </Head>
-      <div className="h-[100vh] w-[10%] bg-green-300"></div>
+      {/* <div className="h-[100vh] w-[10%] bg-green-300"></div> */}
       <div 
         className="flex flex-col items-center justify-start
-         w-[80%] h-[80vh] overflow-scroll no-scrollbar gap-3 py-5">
+         w-full h-[80vh] overflow-scroll no-scrollbar gap-3 py-5">
           {messages.map(m => (
           <div key={m.id} className={`text-center text-[20px] p-3 w-full font-extrabold
            ${m.role === 'assistant' ? 'bg-slate-500' : ''}`}
@@ -31,11 +31,12 @@ export default function Chat() {
             <h1 >{m.content}</h1>
           </div>
         ))}
-        {messages.length === 0 && <h1 className="sm:text-[35px] md:text-[30px] md:max-w-[400px] sm:max-w-none max-w-[250px] text-[20px] text-center absolute bottom-[50%]">
-           Welcome to AnasGPT How Can i Help You Today? 
+        {messages.length === 0 && <h1 className="sm:text-[30px] md:text-[50px] md:max-w-[500px] sm:max-w-none max-w-[250px] text-[40px] text-center absolute bottom-[50%]">
+           Welcome to <strong>AnasGPT</strong> <br/> Ask me anything! 
            </h1>}
-        <div ref={bottomRef}></div>
-        <form onSubmit={handleSubmit} className="absolute sm:bottom-16 bottom-24 w-[50%]">
+        <div ref={bottomRef}></div>        
+      </div>      
+      <form onSubmit={handleSubmit} className=" w-[50%]">
           <TextField
           fullWidth
           placeholder='Ask Me Anything'
@@ -43,8 +44,7 @@ export default function Chat() {
             onChange={handleInputChange}
           />
       </form>
-      </div>      
-      <div className="h-[100vh] w-[10%] bg-green-300"></div>
+      {/* <div className="h-[100vh] w-[10%] bg-green-300"></div> */}
     </div>
   )
 }
