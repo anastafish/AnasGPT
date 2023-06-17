b3028b6cabfdd5b457d8cdab63595b1762fea963
anastafish <anastafish24@gmail.com>
2023-06-17 19:53:47 +0300

    added adseense ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script 
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3647518262867023"
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+          /> 
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useChat } from 'ai/react'
 import { TextField } from "@mui/material"
 import { Oswald } from 'next/font/google'
 import Head from "next/head"
+import AdBanner from "@/components/AdBanner"
 
 const oswald = Oswald({subsets:['latin']})
 
@@ -16,35 +17,40 @@ export default function Chat() {
   }, [messages]);
 
   return (
-    <div className="flex flex-col items-center justify-center w-[100vw]">
+    <div className="flex flex-row items-center justify-center w-[100vw]">      
       <Head>
-        <title>AnasGPT</title>
+        <title>AnasGPT</title>               
       </Head>
-      {/* <div className="h-[100vh] w-[10%] bg-green-300"></div> */}
-      <div 
-        className="flex flex-col items-center justify-start
-         w-full h-[80vh] overflow-scroll no-scrollbar gap-3 py-5">
-          {messages.map(m => (
-          <div key={m.id} className={`text-center text-[20px] p-3 w-full font-extrabold
-           ${m.role === 'assistant' ? 'bg-slate-500' : ''}`}
-           >
-            <h1 >{m.content}</h1>
-          </div>
-        ))}
-        {messages.length === 0 && <h1 className="sm:text-[30px] md:text-[50px] md:max-w-[500px] sm:max-w-none max-w-[250px] text-[40px] text-center absolute bottom-[50%]">
-           Welcome to <strong>AnasGPT</strong> <br/> Ask me anything! 
-           </h1>}
-        <div ref={bottomRef}></div>        
-      </div>      
-      <form onSubmit={handleSubmit} className=" w-[50%]">
-          <TextField
-          fullWidth
-          placeholder='Ask Me Anything'
-            value={input}
-            onChange={handleInputChange}
-          />
-      </form>
-      {/* <div className="h-[100vh] w-[10%] bg-green-300"></div> */}
+      <AdBanner 
+        data-ad-slot="7434970023"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+      <div className="flex flex-col items-center justify-center w-full">
+        <div
+          className="flex flex-col items-center justify-start
+           w-full h-[80vh] overflow-scroll no-scrollbar gap-3 py-5">
+            {messages.map(m => (
+            <div key={m.id} className={`text-center text-[20px] p-3 w-full font-extrabold
+             ${m.role === 'assistant' ? 'bg-slate-500' : ''}`}
+             >
+              <h1 >{m.content}</h1>
+            </div>
+          ))}
+          {messages.length === 0 && <h1 className="sm:text-[30px] md:text-[50px] md:max-w-[500px] sm:max-w-none max-w-[250px] text-[40px] text-center absolute bottom-[50%]">
+             Welcome to <strong>AnasGPT</strong> <br/> Ask me anything!
+             </h1>}
+          <div ref={bottomRef}></div>
+        </div>
+        <form onSubmit={handleSubmit} className=" w-[50%]">
+            <TextField
+            fullWidth
+            placeholder='Ask Me Anything'
+              value={input}
+              onChange={handleInputChange}
+            />
+        </form>
+      </div>
     </div>
   )
 }
