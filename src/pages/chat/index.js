3eb30b5cb95b26f0cd3c5bfe33e31cad5c503e2a
anastafish <anastafish24@gmail.com>
2023-06-21 00:11:43 +0300

    added a button to submit the propmt

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -58,16 +58,29 @@ export default function Chat() {
             handleSubmit(e)
             setFinished(false)  
           }}
-          className=" w-[70%]">
+          className=" w-[70%] flex">
             <TextField
             focused={true}
             autoFocus={true}
+            variant='outlined'
             disabled={finished ? false : true}
             fullWidth
+            style={{borderTopRightRadius:'0px', borderBottomRightRadius:'0px'}}
             placeholder={`${finished ? 'Ask Me Anything' : 'Wait...'}`}
               value={input}
               onChange={handleInputChange}
             />
+          <button className="flex items-center justify-center">
+          <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+              <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
+              </svg>
+            </div>
+          </div>
+          <span className="sm:block hidden">Send</span>
+        </button>
         </form>
       </div>
     </div>
