aa8a339bb630fd10e890e0b2f42bc597679beef0
anastafish <anastafish24@gmail.com>
2023-06-21 00:42:37 +0300

    bug fixes

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -22,7 +22,7 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit} = useChat({
     onFinish:() => setFinished(true),
     onError:() => (err => console.log(err)),
-    initialMessages:[{role:'user', content:`you are an assistent to a user named ${user.displayName.split(' ')[0]}`}]
+    initialMessages:[{role:'user', content:`you are an assistent to a user named ${user ? user.displayName.split(' ')[0] : ''}`}]
   })
 
   const bottomRef = useRef(null);
@@ -74,8 +74,8 @@ export default function Chat() {
             className={`flex items-center justify-center ${finished && input ? 'bg-[rgb(65 105 225)]' : 'bg-gray-600'}`}
             disabled={finished && input ? false : true}
             >
-          <div class="svg-wrapper-1">
-            <div class="svg-wrapper">
+          <div className="svg-wrapper-1">
+            <div className="svg-wrapper">
               <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
